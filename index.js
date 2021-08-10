8e47c639f377fd73898fba04d4c661b6fa0a9b43
Tamiris Vallim <tamirisvallim@gmail.com>
2021-08-10 18:53:01 -0300

    snapCrakle resolvido

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function SnapCrackle(maxValue) {
+
+    let odd = "Snap"
+    let m = "Crackle"
+    let oddM = "SnapCrackle"
+    let number = []
+
+    for (n = 1; n <= maxValue; n++) {
+       
+        if (n % 2 !== 0 && n % 5 !==0) {
+            number.push(odd)
+    }
+        if (n % 5 === 0 && n % 2 === 0) {
+            number.push(m)
+    }
+        if (n % 5 === 0 && n % 2 !== 0) {
+            number.push(oddM)
+    }
+        if (n % 5 !== 0 && n % 2 === 0) {
+            number.push(n)
+    }
+    }
+    return number.join(", ")
+}
+console.log(SnapCrackle(15))
